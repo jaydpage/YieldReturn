3383ef4d0494bf99619f3aae0d655a432a6bc3c8
Jayd Page <jayd.page@chillisoft.co.za>
2019-11-15 16:40:52 +0200

    Add tests around how yield works with delays

---
+++ YieldReturn/Delay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace YieldReturn
+{
+  public class Delay
+  {
+    public IEnumerable<int> GetStuff()
+    {
+      var result = new List<int>();
+
+      for (var x = 0; x < 10; x++)
+      {
+        Thread.Sleep(1000);
+        result.Add(x);
+      }
+
+      return result;
+    }
+
+    public IEnumerable<int> YieldStuff()
+    {
+      for (var x = 0; x < 10; x++)
+      {
+        Thread.Sleep(1000);
+        yield return x;
+      }
+    }
+  }
+}
---
+++ YieldReturn/DelayTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace YieldReturn
+{
+  [TestFixture]
+  public class DelayTests
+  {
+    [TestFixture]
+    public class GetStuff
+    {
+      [Test]
+      public void ShouldReturnAllResultsOfIterationAtTheEnd()
+      {
+        // Arrange
+        var sut = CreateSut();
+        var sw = new Stopwatch();
+        // Act
+        sw.Start();
+
+        foreach (var r in sut.GetStuff())
+        {
+          sw.Stop();
+          // Assert
+          sw.Elapsed.Seconds.Should().BeGreaterOrEqualTo(10);
+        }
+      }
+    }
+
+    [TestFixture]
+    public class YieldStuff
+    {
+      [Test]
+      public void ShouldReturnResultsOfIterationOneAtATime()
+      {
+        // Arrange
+        var sut = CreateSut();
+        var sw = new Stopwatch();
+        // Act
+        sw.Start();
+
+        foreach (var r in sut.YieldStuff())
+        {
+          // Assert
+          sw.Elapsed.Seconds.Should().BeLessOrEqualTo(r + 1);
+        }
+        sw.Stop();
+      }
+    }
+
+    private static Delay CreateSut()
+    {
+      return new Delay();
+    }
+  }
+}
